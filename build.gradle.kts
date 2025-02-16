944b20da44c2a4573b5afa79d514b90a430a831a
人間工作 <coolxiaom95@gmail.com>
2025-02-16 11:22:49 +0800

    enhance gradle build script

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    java
+}
+
+group = "top.mrxiaom"
+version = "1.0.0"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/groups/public/")
+    maven("https://jitpack.io")
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+}
+
+@Suppress("VulnerableLibrariesLocal")
+dependencies {
+    compileOnly("org.spigotmc:spigot-api:1.13.2-R0.1-SNAPSHOT")
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7")
+    compileOnly("me.clip:placeholderapi:2.9.2")
+    compileOnly(files("Residence4.9.0.5.jar"))
+}
+
+val targetJavaVersion = 8
+java {
+    val javaVersion = JavaVersion.toVersion(targetJavaVersion)
+    sourceCompatibility = javaVersion
+    targetCompatibility = javaVersion
+    if (JavaVersion.current() < javaVersion) {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(targetJavaVersion))
+    }
+}
+
+tasks {
+    withType<JavaCompile>().configureEach {
+        if (targetJavaVersion >= 10 || JavaVersion.current().isJava10Compatible) {
+            options.release.set(targetJavaVersion)
+        }
+    }
+
+    processResources {
+        duplicatesStrategy = DuplicatesStrategy.INCLUDE
+        from(sourceSets.main.get().resources.srcDirs) {
+            expand("version" to project.version)
+            include("plugin.yml")
+        }
+    }
+}
